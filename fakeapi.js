c8b81deff4b88a1aeeef9ab43ceeb2f1b4c0430c
a01659224B <a01659224@tec.mx>
2023-08-02 02:31:47 -0600

    Create fakeapi.js

---
+++ fakeapi.js
@@ -0,0 +1,8 @@
+
+  { id: 1, correo: 'juan.perez@example.com', password: 'contraseña123' }
+  { id: 2, correo: 'maria.gomez@example.com', password: '12345678' }
+  { id: 3, correo: 'carlos.rodriguez@example.com', password: 'password123' }
+
+
+
+
